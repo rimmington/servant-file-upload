714e3dff1c8bd8d0c003559a0d50e1aeb5b6aa28
Rhys <rimmington@gmail.com>
2016-02-16 11:27:29 +1100

    FIX: Implement route fall-through properly

--- servant-file-upload-server/src/Servant/FileUpload/Server.hs
+++ servant-file-upload-server/src/Servant/FileUpload/Server.hs
@@ -1,10 +1,12 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
+{-# OPTIONS_GHC -fno-warn-orphans #-}
 
 module Servant.FileUpload.Server (
       UploadedFile, inputName, fileInfo, fileName, fileContentType, filePath
@@ -15,18 +17,17 @@ import Control.Monad.Trans.Resource (runResourceT, withInternalState)
 import Data.Aeson (FromJSON, eitherDecodeStrict)
 import Data.ByteString (ByteString)
 import qualified Data.ByteString.Char8 as BS8
-import qualified Data.ByteString.Lazy.Char8 as BSL8
 import Data.Proxy (Proxy (..))
-import Data.Text.Encoding (encodeUtf8)
 import Lens.Micro (Lens', lens)
 import Lens.Micro.TH (makeLenses)
+import Network.HTTP.Types.Header (hContentType)
+import Network.Wai (requestHeaders)
 import Network.Wai.Parse (FileInfo, tempFileBackEnd, parseRequestBody)
 import qualified Network.Wai.Parse as P
-import Servant (HasServer (..), ServantErr (..), (:>), err400)
+import Servant (HasServer (..), (:>))
 import Servant.FileUpload.API (MultiPartData (..), MultiPartBody)
 import Servant.FileUpload.Internal (primaryBodyKey)
-import Servant.Server.Internal (succeedWith)
-import Servant.Server.Internal.ServantErr (responseServantErr)
+import Servant.Server.Internal (RouteMismatch (..), failWith)
 
 data UploadedFile = UploadedFile { _inputName :: ByteString, _fileInfo :: FileInfo FilePath }
 
@@ -51,10 +52,12 @@ instance (MultiPartData a, FromJSON (PrimaryBody a), HasServer sublayout)
         a UploadedFile -> ServerT sublayout m
 
     route Proxy subserver req respond = withBackend $ \b -> do
-        (params, fs) <- parseRequestBody b req
-        case parse params fs of
-            Left e   -> respond . succeedWith . responseServantErr $ err400 { errBody = BSL8.pack e }
-            Right sa -> route (Proxy :: Proxy sublayout) (subserver sa) req respond
+        let contentTypeH = lookup hContentType $ requestHeaders req
+        case P.parseContentType <$> contentTypeH of
+            Just ("multipart/form-data", _) -> uncurry parse <$> parseRequestBody b req >>= \case
+                Left e   -> respond . failWith $ InvalidBody e
+                Right sa -> route (Proxy :: Proxy sublayout) (subserver sa) req respond
+            _ -> respond $ failWith UnsupportedMediaType
       where
         withBackend f = runResourceT . withInternalState $ f . tempFileBackEnd
         parse params fs = do
