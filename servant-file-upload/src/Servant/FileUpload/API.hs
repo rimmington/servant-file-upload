e47be3690acf7164d840c21e1734d38194bd20cf
Rhys <rimmington@gmail.com>
2016-11-01 14:41:16 +1100

    ENHANCE: HasLink instance for MultiPartBody

--- servant-file-upload/src/Servant/FileUpload/API.hs
+++ servant-file-upload/src/Servant/FileUpload/API.hs
@@ -1,6 +1,13 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
 
 module Servant.FileUpload.API where
+
+import Data.Proxy (Proxy (..))
+import Servant.API ((:>))
+import Servant.Utils.Links (HasLink (..))
 
 data MultiPartBody (b :: * -> *)
 
@@ -8,3 +15,7 @@ class MultiPartData (a :: * -> *) where
     type PrimaryBody a :: *
     parseMultiPartData :: PrimaryBody a -> [b] -> Either String (a b)
     toMultiPartData :: a b -> (PrimaryBody a, [b])
+
+instance (HasLink sub) => HasLink (MultiPartBody a :> sub) where
+    type MkLink (MultiPartBody a :> sub) = MkLink sub
+    toLink _ = toLink (Proxy :: Proxy sub)
