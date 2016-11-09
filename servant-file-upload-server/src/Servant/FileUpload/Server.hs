fc568cb03e42a6345e42151386fe5004521e6406
Rhys <rimmington@gmail.com>
2016-11-10 10:31:45 +1100

    ENHANCE: Use Path over FilePath

--- servant-file-upload-server/src/Servant/FileUpload/Server.hs
+++ servant-file-upload-server/src/Servant/FileUpload/Server.hs
@@ -7,6 +7,7 @@
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# OPTIONS_GHC -fno-warn-orphans #-}
@@ -21,6 +22,7 @@ import Control.Monad.Except (ExceptT (..), runExceptT)
 import Control.Monad.IO.Class (liftIO)
 import Control.Monad.Trans.Resource (runResourceT, withInternalState)
 import Data.Aeson (FromJSON, eitherDecodeStrict)
+import Data.Bifunctor (first)
 import Data.ByteString (ByteString)
 import qualified Data.ByteString.Char8 as BS8
 import qualified Data.ByteString.Lazy.Char8 as BSL8
@@ -31,13 +33,14 @@ import Network.HTTP.Types.Header (hContentType)
 import Network.Wai (Request, requestHeaders)
 import Network.Wai.Parse (FileInfo, tempFileBackEnd, parseRequestBodyEx, defaultParseRequestBodyOptions)
 import qualified Network.Wai.Parse as P
+import Path (Path, Abs, File, parseAbsFile)
 import Servant (HasServer (..), (:>))
 import Servant.FileUpload.API (MultiPartData (..), MultiPartBody)
 import Servant.FileUpload.Internal (primaryBodyKey)
-import Servant.Server (Handler, err400, err415, errBody)
+import Servant.Server (Handler, err400, err415, err500, errBody)
 import Servant.Server.Internal (addBodyCheck, withRequest, delayedFailFatal)
 
-data UploadedFile = UploadedFile { _inputName :: ByteString, _fileInfo :: FileInfo FilePath }
+data UploadedFile = UploadedFile { _inputName :: ByteString, _fileInfo :: FileInfo (Path Abs File) }
                   deriving (Show, Eq)
 
 data TempFileBacked a = TempFileBacked (forall b. (a UploadedFile -> Handler b) -> Handler b)
@@ -58,7 +61,7 @@ fileContentType :: Lens' UploadedFile ByteString
 fileContentType = lens (P.fileContentType . _fileInfo)
                        (\s b -> s { _fileInfo = (_fileInfo s) { P.fileContentType = b } })
 
-filePath :: Lens' UploadedFile FilePath
+filePath :: Lens' UploadedFile (Path Abs File)
 filePath = lens (P.fileContent . _fileInfo)
                 (\s b -> s { _fileInfo = (_fileInfo s) { P.fileContent = b } })
 
@@ -76,15 +79,21 @@ instance (MultiPartData a, FromJSON (PrimaryBody a), HasServer api context)
                 Just ("multipart/form-data", _) -> pure $ go req
                 _                               -> delayedFailFatal err415
         withBackend f = runResourceT . withInternalState $ f . tempFileBackEnd
-        parse params fs = do
-            json <- maybe (Left "Missing primary body") Right $ lookup primaryBodyKeyBS params
-            a <- eitherDecodeStrict json :: Either String (PrimaryBody a)
-            parseMultiPartData a (uncurry UploadedFile <$> fs)
         go :: Request -> TempFileBacked a
-        go req = TempFileBacked $ \f -> ExceptT . withBackend $ \b ->
-            uncurry parse <$> parseRequestBodyEx defaultParseRequestBodyOptions b req >>= \case
-                Left e  -> pure . Left $ err400 { errBody = BSL8.pack e }
-                Right a -> runExceptT $ f a
+        go req = TempFileBacked $ \f -> ExceptT . withBackend $ \b -> runExceptT $ do
+            (params, fs) <- liftIO $ parseRequestBodyEx defaultParseRequestBodyOptions b req
+            bdy <- withEither mk400 $ parseBody params
+            fs' <- withEither (const $ err500 { errBody = "Uploaded file error" }) $ traverse parseFileName fs
+            a <- withEither mk400 $ parseMultiPartData bdy fs'
+            f a
+        mk400 e = err400 { errBody = BSL8.pack e }
+        withEither f = ExceptT . pure . first f
+        parseBody params = do
+            json <- maybe (Left "Missing primary body") Right $ lookup primaryBodyKeyBS params
+            eitherDecodeStrict json :: Either String (PrimaryBody a)
+        parseFileName (n, P.FileInfo{..}) = do
+            path <- parseAbsFile fileContent
+            pure . UploadedFile n $ P.FileInfo{fileContent = path, ..}
 
 primaryBodyKeyBS :: ByteString
 primaryBodyKeyBS = BS8.pack primaryBodyKey
